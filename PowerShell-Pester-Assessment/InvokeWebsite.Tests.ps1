22a72c93fd6ee03497c235093d0196dc48ff91f4
vijayalakshmik <kamathvijayalakshmi@gmail.com>
2019-05-18 20:56:08 +0530

    Powershell Pester demostration Assessment

---
+++ PowerShell-Pester-Assessment/InvokeWebsite.Tests.ps1
@@ -0,0 +1,20 @@
+﻿# Tests
+Describe 'Validate InvokeWebsite response' {
+    $response = Main
+
+    It 'Test to validate response not null' {
+        $response | Should not BeNullOrEmpty
+    }
+    
+    It 'Test to validate quote not null' {
+        $response.quote | Should not BeNullOrEmpty
+    }
+
+    It 'Test which validates author not null' {
+        $response.author | Should not BeNullOrEmpty
+    }
+
+    It 'Test which validates category not null' {
+        $response.category | Should not BeNullOrEmpty
+    }
+}
---
+++ PowerShell-Pester-Assessment/InvokeWebsite.ps1
@@ -0,0 +1,26 @@
+﻿#
+# Main function to demonstrate REST call for given,
+#   uri - https://andruxnet-random-famous-quotes.p.mashape.com/
+#   Header - X-Mashape-Key : bckcFeIRo5mshMpc3C4ug7tmUplYp1jYdDYjsnBy5UQQpYB6dE
+#
+function Main 
+{
+    $uri = "https://andruxnet-random-famous-quotes.p.mashape.com/"
+    $headers = @{}
+    $headers.Add("X-Mashape-Key", "bckcFeIRo5mshMpc3C4ug7tmUplYp1jYdDYjsnBy5UQQpYB6dE")
+    $response = Invoke-Website -Uri $uri -Headers $headers
+    
+    return $response
+}
+
+#
+# Invokes the given website and returns the response.
+#
+function Invoke-Website
+{
+    Param($Uri, $Headers)
+
+    $response = Invoke-RestMethod -Uri $uri -Method Get -Headers $headers
+
+    return $response
+}
